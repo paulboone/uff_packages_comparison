b986281aa1fecfdd416c1c17e1526499daf17b69
Paul Boone <narcissus.pong@gmail.com>
2018-12-19 22:16:01 -0500

    add missing line to open-babel import script; add output-files flag and output

--- openbabel_type_mofs.py
+++ openbabel_type_mofs.py
@@ -10,14 +10,14 @@ from openbabel import OBMol, OBConversion
 from openbabel import OBMolAtomIter, OBForceField
 from angstrom import Molecule
 
-def type_mof(filename, output_dir, ff="uff"):
+def type_mof(filename, output_dir, ff="uff", output_files=True):
 
     obconversion = OBConversion()
     obconversion.SetInAndOutFormats("cif", "xyz")
     obmol = OBMol()
 
     # Read MOF file and unit cell and write xyz file
-    obconversion.ReadFile(obmol, os.path.join(mof_dir, filename))
+    obconversion.ReadFile(obmol, filename)
     unitcell = openbabel.toUnitCell(obmol.GetData(openbabel.UnitCell))
     uc = [unitcell.GetA(), unitcell.GetB(), unitcell.GetC(), unitcell.GetAlpha(), unitcell.GetBeta(), unitcell.GetGamma()]
     obconversion.WriteFile(obmol, 'mof_tmp.xyz')
@@ -25,6 +25,8 @@ def type_mof(filename, output_dir, ff="uff"):
     # Replicate unit cell using angstrom
     mol = Molecule(read='mof_tmp.xyz')
     mol.set_cell(uc)
+    n_atoms = len(mol.atoms)
+
     mol333 = mol.replicate([3, 3, 3], center=True)
     print(mol333.cell)
     mol333.write('mof333.cif', cell=mol333.cell.to_list())
@@ -42,6 +44,12 @@ def type_mof(filename, output_dir, ff="uff"):
         if atom_idx >= n_atoms * 13 and atom_idx < n_atoms * 14:
             ff_atom_type = obatom.GetData("FFAtomType").GetValue()
             types.append(ff_atom_type)
+
+    if output_files:
+        mof_name = os.path.splitext(os.path.basename(filename))[0]
+        with open(os.path.join(output_dir, mof_name + "-obabel.log"), 'w') as f:
+            f.write("NOTE: types order is the same as the CIF input file.\n")
+            f.write("types= %s" % str(types))
 
     uniq_types = sorted(set(types))
     return [str(i) for i in uniq_types]
